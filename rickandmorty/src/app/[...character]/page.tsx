e8253d47f774a2359bca79f8a752f9ceb4dbebff
RoJosGaRis <roberto.jgr03@gmail.com>
2024-05-14 14:14:04 -0600

    chore: Add constants, functions, and types for cookie handling

--- rickandmorty/src/app/[...character]/page.tsx
+++ rickandmorty/src/app/[...character]/page.tsx
@@ -1,69 +1,51 @@
-"use client";
-import CharacterCard from "../components/CharacterCard";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import { Character } from "../components/CharacterCard";
+import Image from 'next/image'
+import { getCharacter } from '../../api/characterRequests'
 
-// async function getData() {
-//   fetch("https://rickandmortyapi.com/api/character")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       return data.results;
-//     });
-// }
-
-export default function Home({ params }: { params: { character: string } }) {
-  const [characterInfo, setCharacterInfo] = useState<Character>();
-
-  useEffect(() => {
-    fetch(`https://rickandmortyapi.com/api/character/${params.character}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCharacterInfo(data);
-        console.log(params.character);
-      });
-  }, []);
-
+export default async function Home() {
+  const characterInfo = await getCharacter(1)
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className='flex w-full items-center justify-center'>
       {characterInfo && (
-        <div className="w-6/12 h-[100%] min-h-[100vh] bg-slate-100 flex justify-start items-center flex-col">
-          <div className="h-[200px] w-[200px] bg-slate-300 m-10 flex flex-col items-center justify-center rounded-full shadow-lg">
+        <div className='flex h-[100%] min-h-[100vh] w-6/12 flex-col items-center justify-start bg-slate-100'>
+          <div className='m-10 flex h-[200px] w-[200px] flex-col items-center justify-center rounded-full bg-slate-300 shadow-lg'>
             <Image
               src={characterInfo.image}
               alt={characterInfo.name}
               width={300}
               height={300}
-              className="rounded-full"
+              className='rounded-full'
             />
           </div>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Name:</strong> {characterInfo.name}
           </h1>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Status:</strong> {characterInfo.status}
           </h1>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Gender:</strong> {characterInfo.gender}
           </h1>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Origin:</strong> {characterInfo.origin.name}
           </h1>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Location:</strong> {characterInfo.location.name}
           </h1>
-          <h1 className="text-black">
+          <h1 className='text-black'>
             <strong>Episodes:</strong>
           </h1>
-          <div className="flex gap-2 flex-wrap px-20 py-2">
-            {characterInfo.episode.map((episode) => (
-              <h2 className="text-black bg-green-200 w-[25px] rounded-full text-center">
-                {episode.split("https://rickandmortyapi.com/api/episode/")[1]}
+          <div className='flex flex-wrap gap-2 px-20 py-2'>
+            {characterInfo.episode.map((episode, index) => (
+              <h2
+                key={index}
+                className='w-[25px] rounded-full bg-green-200 text-center text-black'
+              >
+                {episode.split('https://rickandmortyapi.com/api/episode/')[1]}
               </h2>
             ))}
           </div>
         </div>
       )}
     </div>
-  );
+  )
 }
